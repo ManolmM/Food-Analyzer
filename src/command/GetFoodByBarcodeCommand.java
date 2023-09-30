bbaea4cdb05c2bdfb75882b2df853d38a9b613e8
ManolmM <72990338+ManolmM@users.noreply.github.com>
2023-09-30 22:37:12 +0300

    4 new classes

---
+++ src/command/GetFoodByBarcodeCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+import java.util.List;
+
+public class GetFoodByBarcodeCommand implements Command {
+
+    public GetFoodByBarcodeCommand() {
+
+    }
+    @Override
+    public void executeRequest(List<String> command) {
+
+    }
+}
---
+++ src/command/GetFoodCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+import java.util.List;
+
+public class GetFoodCommand implements Command {
+
+    public GetFoodCommand() {
+
+    }
+    @Override
+    public void executeRequest(List<String> command) {
+
+    }
+}
